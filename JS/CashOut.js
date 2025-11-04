51fb813de78fc53700ca0f2e55fddfff5c9ca112
mdsabbir112 <anikmullah112@gmail.com>
2025-11-05 02:05:28 +0600

    added the next things

--- JS/CashOut.js
+++ JS/CashOut.js
@@ -1,3 +1,5 @@
+// Select the form first
+let form1 = document.getElementById("cashout-form");
 
 
 let cashoutBtn = document.getElementById('login-btn-cashout');
@@ -5,6 +7,21 @@ let cashoutBtn = document.getElementById('login-btn-cashout');
     //   Remove the default reload action
         event.preventDefault();
       
+        // ✅ Check HTML5 required validation manually
+  if (!form1.checkValidity()) {
+    // Check if the form is valid with correct input types
+    form1.reportValidity(); // Triggers built-in browser messages (like "Please fill out this field")
+    return; // Stop further code execution
+  }
+
+        // Get the value of the Account Number and Pin fields
+        let AccountNumberCashout =  document.getElementById('Account-number-cashout').value;
+        let pinNumberCashout = document.getElementById('pin-number-cashout').value;
+
+    if(AccountNumberCashout.length == 11){
+            if(pinNumberCashout.length == 4){
+               alert("Cash Out Successful. " );
+
         let cashoutAmount = document.getElementById('cashout-amount').value;
         // get the main balance
         let mainBalance1 = document.getElementById('main-balance').innerText;
@@ -16,4 +33,14 @@ let cashoutBtn = document.getElementById('login-btn-cashout');
         
         // Insert the main value into the main Account Balance 
         document.getElementById('main-balance').innerText = mainCashOut;
-    })
+   
+            }
+             else {
+                alert("Invalid Pin Number");
+            }
+    }
+    else {
+        alert("Invalid Account Number");
+    }
+
+    });
